c696a5b35c3dfb194f342d42e94b34dd14f9fb6b
cntct24ronald-del <cntct24.ronald@gmail.com>
2026-08-06 12:21:05 +0530

    Update greeting message in home route

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello Google App Engine</h1>"
+    return "<h1>Hello Google App Engine!</h1>"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
